ce2ddec59b2b4fe95de66a4784180ae25f56c528
Shreeya Sharma <144382444+shreeyasharma09@users.noreply.github.com>
2025-04-12 23:09:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import TodoList from './components/TodoList';
 const App = () => {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (name, priority) => {
+    const newTodo = {
+      id: Date.now(),
+      name,
+      priority
+    };
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
   return (
     <div>
       <h1>ToDo List</h1>
-      <AddTodo />
-      <TodoList />
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 };
